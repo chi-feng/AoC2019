9ae621c13cca1746385366e22733c17b95de3c8d
Chi Feng <email.chi.feng@gmail.com>
2019-12-04 02:22:00 -0500

    Updated day4

--- day4/day4.py
+++ day4/day4.py
@@ -3,18 +3,11 @@ from collections import Counter
 lower = 273025
 upper = 767253
 
-digits = '0123456789'
+def is_valid1(s):
+	return list(s) == sorted(s) and any(count >= 2 for count in Counter(s).values())
 
-def is_valid_part1(s):
-	return all(i <= j for i, j in zip(s, s[1:])) and any(count >= 2 for count in Counter(s).values())
+def is_valid2(s):
+	return list(s) == sorted(s) and any(count == 2 for count in Counter(s).values())
 
-def is_valid_part2(s):
-	return all(i <= j for i, j in zip(s, s[1:])) and any(count == 2 for count in Counter(s).values())
-
-valid = [n for n in range(lower, upper + 1) if is_valid_part1(str(n))]
-print(valid)
-print(len(valid))
-
-valid = [n for n in range(lower, upper + 1) if is_valid_part2(str(n))]
-print(valid)
-print(len(valid))
+print(sum(1 for n in range(lower, upper) if is_valid1(str(n))))
+print(sum(1 for n in range(lower, upper) if is_valid2(str(n))))
