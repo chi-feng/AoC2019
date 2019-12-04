53c29fdae9fd7d8b975e533f7cf6cacbf8f0e0f3
Chi Feng <email.chi.feng@gmail.com>
2019-12-04 00:57:15 -0500

    Updated day4

--- day4/day4.py
+++ day4/day4.py
@@ -1,26 +1,20 @@
+from collections import Counter
+
 lower = 273025
 upper = 767253
 
 digits = '0123456789'
 
 def is_valid_part1(s):
-	# non decreasing
-	if any(s[i] < s[i-1] for i in range(1, len(s))):
-		return False
-	# contains the same digit (at least) twice
-	return any(2 * d in s for d in digits)
+	return all(i <= j for i, j in zip(s, s[1:])) and any(count >= 2 for count in Counter(s).values())
 
 def is_valid_part2(s):
-	# non decreasing
-	if any(s[i] < s[i-1] for i in range(1, len(s))):
-		return False
-	# contains the same digit twice, but not three (or more) times
-	return any(2 * d in s and 3 * d not in s for d in digits)
+	return all(i <= j for i, j in zip(s, s[1:])) and any(count == 2 for count in Counter(s).values())
 
-valid = [number for number in range(lower, upper + 1) if is_valid_part1(str(number))]
-# print(valid)
+valid = [n for n in range(lower, upper + 1) if is_valid_part1(str(n))]
+print(valid)
 print(len(valid))
 
-valid = [number for number in range(lower, upper + 1) if is_valid_part2(str(number))]
-# print(valid)
+valid = [n for n in range(lower, upper + 1) if is_valid_part2(str(n))]
+print(valid)
 print(len(valid))
