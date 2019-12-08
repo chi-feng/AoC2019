8f123d5a4bce08d6772bcb93c02e4f1439f83fab
Chi Feng <email.chi.feng@gmail.com>
2019-12-08 00:38:05 -0500

    Day 8

---
+++ day8/day8.py
@@ -0,0 +1,48 @@
+with open("input.txt") as file:
+	s = file.readline().strip()
+
+n = 6
+m = 25
+
+nlayers = len(s) // (n * m)
+
+# part 1
+
+layers = []
+for i in range(nlayers):
+	start = i * n * m
+	end = start + n * m
+	layer = s[start:end]
+	layers.append(list(map(int, layer)))
+
+zeros = [layer.count(0) for layer in layers]
+index = zeros.index(min(zeros))
+print(layers[index].count(1) * layers[index].count(2))
+
+# part 2
+
+image = [2] * (n * m) # 2 means pixel is transparent
+
+for i in range(nlayers):
+	offset = i * n * m
+	for j in range(n * m):
+		pixel = int(s[offset + j])
+		if image[j] == 2 and (pixel == 0 or pixel == 1):
+			image[j] = pixel
+
+# print image to terminal
+for i in range(n):
+	for j in range(m):
+		if image[i*m+j] == 1:
+			print('█', end="")
+		else:
+			print(' ', end="")
+	print('')
+
+# plot image
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+plt.spy(np.reshape(np.array(image), (n, m)))
+plt.savefig('image.png')
