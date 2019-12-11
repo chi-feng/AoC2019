5b5dc4764a39d203dfbb76928812c34427124b1c
Chi Feng <email.chi.feng@gmail.com>
2019-12-11 18:41:04 -0500

    Remove trailing whitespace

--- shared/test_intcode.py
+++ shared/test_intcode.py
@@ -45,8 +45,8 @@ def test_relative_mode_alloc():
 
 
 def test_conditionals():
-    """The above example program uses an input instruction to ask for a single number. 
-       The program will then output 999 if the input value is below 8, output 1000 if the 
+    """The above example program uses an input instruction to ask for a single number.
+       The program will then output 999 if the input value is below 8, output 1000 if the
        input value is equal to 8, or output 1001 if the input value is greater than 8."""
     program = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
     program = list(map(int, program.split(",")))
